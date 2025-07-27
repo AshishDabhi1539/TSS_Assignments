6105b8fb449e0bbd472eabcf07be4225294b3a49
Ashishkumar Jayeshbhai Dabhi <160484924+AshishDabhi1539@users.noreply.github.com>
2025-07-27 09:56:59 +0530

    Delete Account/UIUX Lecture notes.pdf

--- Account/exception_handling/src/com/tss/exception/MinimumBalanceViolationException.java
+++
@@ -1,7 +0,0 @@
-package com.tss.exception;
-
-public class MinimumBalanceViolationException extends Exception {
-	public MinimumBalanceViolationException(String message) {
-        super(message);
-    }
-}
